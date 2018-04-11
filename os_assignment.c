4ef85a10f43575b3a668fe37b81b5360e630d02c
kumariharsha <kharshit95@gmail.com>
2018-04-11 22:55:16 +0530

    5

--- os_assignment.c
+++ os_assignment.c
@@ -1,19 +1,23 @@
 #include<stdio.h>
 #include<pthread.h>
+#include<unistd.h>
 #define MIN_PID 300
 #define MAX_PID 5000
 
-pthread_mutex_t l1;         
+pthread_mutex_t l1;
+int t=1;
 
-struct pidValue             //one structure is declared having two 
+struct pidValue          //one structure created
 {
 	int flag;
 	int pid;
 }p[4701];
-int allocate_map(void)     //this function allocates values to data structure 
+int allocate_map(void)     //allocation of values to data structure
 {
 	int j=0,i;
-	printf("Creating data structure.\n");
+	printf("\nCreating data structure.\n");
+	sleep(2);
+	printf("Data Structure created\n\n");
 	for(i=MIN_PID;i<=MAX_PID;i++)
 	{
 		p[j].pid=i;
@@ -27,10 +31,10 @@ int allocate_map(void)     //this function allocates values to data structure
 	return 1;
 }
 
-int allocate_pid(void)      // this function allocates process id 
+int allocate_pid(void)      //allocation of PID to threads
 {
 	int j=0,i;
-	printf("allocating pid to process\n");
+	printf("requesting a PID for process %d\n",t);
 	for(i=MIN_PID;i<=MAX_PID;i++)
 	{
 		if(p[j].flag==0)
@@ -43,7 +47,7 @@ int allocate_pid(void)      // this function allocates process id
 	return -1;	
 }
 
-int release_pid(int pid)      //this function release process id 
+int release_pid(int pid)      //this function release PID
 {
 	int j=0,i;
 	for(i=MIN_PID;i<MAX_PID;i++)
@@ -52,30 +56,33 @@ int release_pid(int pid)      //this function release process id
 			p[j].flag==0;
 			j++;
 	}
-	printf("Releasing pid %d",pid);
+	printf("Releasing pid %d of process %d\n",pid,t);
 }
-void *crthread()              //this function create,allocate and release pid
+void *crthread()                     //this function call other functions
 {
 	printf("Entering thread function\n");
 	int r=allocate_pid();
-	printf("PID allocated\n");
-	pthread_mutex_lock(&l1);             //mutex lock acquired
-	sleep(7);
+	printf("PID %d allocated to process %d\n",r,t);
+	t++;
+	pthread_mutex_lock(&l1);           //mutex lock acquired
+	sleep(2);
 	
-	pthread_mutex_unlock(&l1);           //mutex lock released
+	pthread_mutex_unlock(&l1);         //mutex lock released
 	release_pid(r);
-	printf("PID released\n");
+	printf("PID released\n\n\n");
 }
 
 int main()
 {
 	pthread_t th[100];
-	int i;
+	int i,n;
+	printf("Enter no of thread u want to create:  ");
+	scanf("%d",&n);             //threads entered by user
 	allocate_map();
-	for(i=0;i<100;i++)                //threads are created
+	for(i=0;i<n;i++)            //threads are created
 	{
-		printf("Allocating PID to thread %d",i+1);
-	pthread_create(&th[i],NULL,crthread,NULL);
-	pthread_join(th[i],NULL);
-    }
+		printf("\nAllocation of PID to process %d started\n",i+1);
+		pthread_create(&th[i],NULL,crthread,NULL);
+		pthread_join(th[i],NULL);
+    	}
 }
